25a3b23b2005a869ff456aac653d9ce7eacd81f3
Nobody17 <24973487+Nobody17@users.noreply.github.com>
2024-05-16 20:20:14 +0200

    theme change

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Ayu Mirage'
+config.color_scheme = 'Catppuccin Mocha'
 
 config.default_cwd = 'C:\\'
 config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
