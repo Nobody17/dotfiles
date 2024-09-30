2778929835f1a449d688921c50580c9c493517eb
Nobody17 <24973487+Nobody17@users.noreply.github.com>
2024-10-01 01:06:33 +0200

    bat and aliases

--- !install/cargo.fish
+++ !install/cargo.fish
@@ -1,7 +1,11 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup update
 
-cargo install fd-find # maybe alias to fd
+cargo install fd-find
+if test -e /usr/bin/fdfind
+    mkdir -p ~/.local/bin
+    ln -s /usr/bin/fdfind ~/.local/bin/fd
+end
 cargo install taplo-cli --locked
 cargo install eza
 cargo install zoxide --locked
--- !install/ubuntu.fish
+++ !install/ubuntu.fish
@@ -1,1 +1,4 @@
 sudo apt install lua5.4
+sudo apt install bat
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
