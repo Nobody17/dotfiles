1e027b7277d399c2e15af833f9a76c22744d6914
Nobody17 <24973487+Nobody17@users.noreply.github.com>
2024-10-01 04:06:11 +0200

    wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -20,32 +20,35 @@ end
 config.color_scheme = 'Catppuccin Mocha'
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  config.default_cwd = 'C:\\'
-  config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
+  -- config.default_cwd = 'C:\\'
+  -- config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
+  config.default_domain = 'WSL:Ubuntu'
 elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
   config.default_prog = { "/bin/fish" }
-  config.hide_tab_bar_if_only_one_tab = true
-  config.background = {
-    {
-      source = {
-        -- File = '/home/yorunai/Pictures/Wallpaper/Wallpaper.png'
-
-      },
-      opacity = 1,
-      height = "Cover",
-      width = "Cover"
-    },
-    {
-      source = {
-        Color = "black",
-      },
-      opacity = 0.7,
-      height = "100%",
-      width = "100%"
-    }
-  }
 end
 
+
+config.background = {
+  -- {
+  --   source = {
+  --     -- File = '/home/yorunai/Pictures/Wallpaper/Wallpaper.png'
+
+  --   },
+  --   opacity = 1,
+  --   height = "Cover",
+  --   width = "Cover"
+  -- },
+  {
+    source = {
+      Color = "black",
+    },
+    opacity = 0.7,
+    height = "100%",
+    width = "100%"
+  }
+}
+
+config.hide_tab_bar_if_only_one_tab = true
 
 local act = wezterm.action
 config.disable_default_key_bindings = true
@@ -67,14 +70,14 @@ config.keys = {
   { key = 'V', mods = 'CTRL',     action = act.PasteFrom 'Clipboard' },
 }
 
-for i = 1, 8 do
-  -- CTRL+ALT + number to activate that tab
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = 'CTRL|ALT',
-    action = act.ActivateTab(i - 1),
-  })
-end
+-- for i = 1, 8 do
+--   -- CTRL+ALT + number to activate that tab
+--   table.insert(config.keys, {
+--     key = tostring(i),
+--     mods = 'CTRL|ALT',
+--     action = act.ActivateTab(i - 1),
+--   })
+-- end
 
 wezterm.on("gui-startup", function(cmd)
   local tab, pane, window = mux.spawn_window(cmd or {})
