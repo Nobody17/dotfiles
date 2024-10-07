41ffdf4d11c8fb7e231535ad8860bf0f45df6d3e
Nobody17 <24973487+Nobody17@users.noreply.github.com>
2024-10-07 09:44:18 +0200

    nvim

--- !install/nvim.fish
+++ !install/nvim.fish
@@ -5,10 +5,3 @@ sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 fish_add_path /opt/nvim-linux64/bin
 sudo rm nvim-linux64.tar.gz
-
-sudo apt install git make unzip gcc xclip
-git clone https://github.com/nvim-lua/kickstart.nvim.git $HOME/.config/nvim
-cd $HOME/.config/nvim
-git pull
-cd -
-nvim
---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,26 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+
+-- Add lazy to the `runtimepath`, this allows us to `require` it.
+---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+require('lazy').setup { import = 'custom/plugins', change_detection = { nofify = false } }
