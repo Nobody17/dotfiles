a527e66195b0ca9c176df6449c729247b314bb20
Nobody17 <24973487+Nobody17@users.noreply.github.com>
2024-10-16 15:10:54 +0200

    fnm

--- fish/.config/fish/conf.d/fnm.fish
+++ fish/.config/fish/conf.d/fnm.fish
@@ -26,3 +26,10 @@ if [ -d "$FNM_PATH" ]
   set PATH "$FNM_PATH" $PATH
   fnm env | source
 end
+
+# fnm
+set FNM_PATH "/home/yorunai/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env | source
+end
