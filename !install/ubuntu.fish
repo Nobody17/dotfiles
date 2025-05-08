a4eecc32b7212f533858f0d6c6478a32aa112ce7
Nobody17 <24973487+Nobody17@users.noreply.github.com>
2025-05-08 17:25:25 +0200

    config

--- !install/ubuntu.fish
+++ !install/ubuntu.fish
@@ -1,8 +1,10 @@
 #!/usr/bin/env fish
 sudo apt update
+sudo apt install build-essential libreadline-dev 
 sudo apt install git curl unzip xclip
-sudo apt install lua5.4
+sudo apt install lua5.4 liblua5.4-dev
 sudo apt install bat
+sudo apt install sqlite3
 sudo apt install python3.12-venv
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
