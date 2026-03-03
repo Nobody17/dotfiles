0d62bc00f1a8db90926c948f3482377f48e2098c
Nobody17 <24973487+Nobody17@users.noreply.github.com>
2026-03-03 15:00:40 +0100

    home to $HOME

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,24 +7,17 @@ function y
     rm -f -- "$tmp"
 end
 
-# Set up fzf key bindings
 fzf --fish | source
 if status is-interactive
   zoxide init fish | source
 end
 starship init fish | source
 enable_transience
-# 1. Enable shell history for IEx
 set -gx ERL_AFLAGS "-kernel shell_history enabled"
 
-# 2. Define ASDF Data Directory (Standard path)
 set -gx ASDF_DATA_DIR "$HOME/.asdf"
-
-# 3. Add shims and asdf bin to path
-# Note: It's better to add the bin folder too so the 'asdf' command itself is found
 fish_add_path "$ASDF_DATA_DIR/bin"
 fish_add_path "$ASDF_DATA_DIR/shims"
-fish_add_path "$HOME/bin"
 
 # 4. Source asdf (Standard way for Fish)
 if test -f "$ASDF_DATA_DIR/asdf.fish"
@@ -46,18 +39,17 @@ alias get_idf=". $HOME/esp/esp-idf/export.fish"
 #Linux
 #set -gxa SSH_ASKPASS "/usr/bin/ssh-askpass"
 
+set -e -g fish_user_paths
+set -gxa PHP_INI_SCAN_DIR "$HOME/.config/herd-lite/bin"
+
 #path
-fish_add_path "/home/yorunai/Programming/software/android-studio/bin/"
-fish_add_path "/home/yorunai/Programming/software/platform-tools/"
+fish_add_path "$HOME/Programming/software/android-studio/bin/"
+fish_add_path "$HOME/Programming/software/platform-tools/"
 fish_add_path "/opt/nvim/bin"
-fish_add_path "/home/yorunai/.local/share/nvim/mason/bin"
+fish_add_path "$HOME/.local/share/nvim/mason/bin"
 fish_add_path "/usr/local/go/bin"
-fish_add_path "/home/yorunai/go/bin"
-fish_add_path "/home/yorunai/.config/herd-lite/bin"
-set -gxa PHP_INI_SCAN_DIR "/home/yorunai/.config/herd-lite/bin"
-set PATH $PATH /home/yorunai/.local/bin
-
-# Prefer custom FFmpeg (xHE-AAC via libfdk_aac)
-set -e -g fish_user_paths            # remove any shadowing global
+fish_add_path "$HOME/go/bin"
+fish_add_path "$HOME/.config/herd-lite/bin"
+fish_add_path "$HOME/bin"
+fish_add_path "$HOME/.local/bin"
 fish_add_path -U --prepend /opt/ffmpeg/bin
-
